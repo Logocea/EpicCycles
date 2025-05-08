a8a466de67ba4d0e7c200b6cdc6946fb11144199
Quest <ecsolticia@gmail.com>
2025-05-08 15:49:31 +0600

    Implemented mousehover over epicycle line detection

--- Scenes/Epicycle/epicycle.gd
+++ Scenes/Epicycle/epicycle.gd
@@ -15,35 +15,49 @@ extends Node2D
 var parent_epicycle_node: Node2D
 
 func update_parent_epicycle(new_path: NodePath) -> void:
-    parent_epicycle = new_path
-    parent_epicycle_node = get_node(parent_epicycle)
+	parent_epicycle = new_path
+	parent_epicycle_node = get_node(parent_epicycle)
 
 func fix_time_passed(delta: float) -> void:
-    time_passed += delta
+	time_passed += delta
 
 func fix_angle() -> void:
-    current_angle = 2.0 * PI * 1.0/float(rotational_period) * time_passed
+	current_angle = 2.0 * PI * 1.0/float(rotational_period) * time_passed
 
 func fix_current_radius() -> void:
-    current_radius = canonical_radius.rotated(current_angle)
+	current_radius = canonical_radius.rotated(current_angle)
 
 func _ready() -> void:
-    if parent_epicycle:
-        update_parent_epicycle(parent_epicycle)
+	if parent_epicycle:
+		update_parent_epicycle(parent_epicycle)
 
 func apply_simulation_logic(delta: float) -> void:
-    if parent_epicycle_node:
-        global_position = parent_epicycle_node.global_position + parent_epicycle_node.current_radius
+	if parent_epicycle_node:
+		global_position = parent_epicycle_node.global_position + parent_epicycle_node.current_radius
 
-    fix_time_passed(delta)
-    fix_angle()
-    fix_current_radius()
+	fix_time_passed(delta)
+	fix_angle()
+	fix_current_radius()
 
 func update_visual_line() -> void:
-    visual_line.points[1] = current_radius
+	visual_line.points[1] = current_radius
+
+func update_collision_shape() -> void:
+	var shape: RectangleShape2D = RectangleShape2D.new()
+	shape.size = Vector2(canonical_radius.length(), 20)
+	var shape_node: CollisionShape2D = $Area2D/CollisionShape2D
+	shape_node.rotation = current_radius.angle()
+	shape_node.position = current_radius/2.0
+	shape_node.shape = shape
 
 func _physics_process(delta: float) -> void:
-    if sim_status.sim_running:
-        apply_simulation_logic(delta)
-        update_visual_line()
-    
+	if sim_status.sim_running:
+		apply_simulation_logic(delta)
+		update_visual_line()
+		update_collision_shape()
+	
+func _on_area_2d_mouse_entered() -> void:
+	$AnimationPlayer.play("line_hovered")
+
+func _on_area_2d_mouse_exited() -> void:
+	$AnimationPlayer.play("RESET")
